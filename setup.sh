9882ad5fb5ee8f2b0cdad3fb22e8291494f93558
JQ <kamahl437@gmail.com>
2018-08-19 14:45:42 -0400

    missing webpack

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ mkdir public
 cd public
 git clone https://github.com/kamahl437/phaser3-typescript.git
 cd phaser3-typescript
+git pull
 npm install
+npm i -g webpack
 npm run webpack --mode production
 cd ../..
 node index.js
